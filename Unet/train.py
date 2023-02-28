7fdaead600cfaec484cbfba447aa45e4cf7e7fe6
Zane <44780287+betterlmy@users.noreply.github.com>
2023-02-28 22:00:51 +0800

    更新1

--- Unet/train.py
+++ Unet/train.py
@@ -7,16 +7,22 @@ from net import Unet
 import os
 import time
 
-device = torch.device("cuda:5" if torch.cuda.is_available() else "cpu")
-print("used device now is",device)
-weight_path = r'params/unet.pth'
-data_dir = r'/Users/zane/Desktop/VOCdevkit/VOC2007' # 数据集使用VOC2007
-out_dir = r'output/'
-
 
 if __name__ == '__main__':
+        
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    print("used device now is",device)
+    weight_path = r'params'
+    if not os.path.exists(weight_path):
+        os.makedirs(weight_path)
+    weight_path = os.path.join(weight_path,"unet.pth")
+    out_dir = r'output/'
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+        
+    data_dir = r'/root/lmy/data/VOCdevkit/VOC2007/' # 数据集使用VOC2007
     dataset = MyDataset(data_dir)
-    dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=28, shuffle=True)
     
     net = Unet().to(device)
     if os.path.exists(weight_path):
@@ -28,7 +34,7 @@ if __name__ == '__main__':
     opt = optim.Adam(net.parameters(),lr=1e-3)
     
     loss_fn = nn.BCELoss()
-    num_epoch = 10
+    num_epoch = 100
     min_loss = 99999.0
     for epoch in range(num_epoch):
         start_time = time.time()
@@ -43,16 +49,16 @@ if __name__ == '__main__':
             train_loss.backward()
             opt.step()
             if (i+1)%10 == 0:
-                print("epoch:{},step:{},loss======>{}".format(epoch,i+1,train_loss.item()))
-                end_time = time.time()
-                print("time cost:{:.2f}".format(end_time-start_time))
+                print("epoch:{},step:{},loss======>{:.4f}".format(epoch,i+1,train_loss.item()))
+
                 _image = img[0]
                 _segment_image = seg_img[0]
                 _out_image = out[0]
                 img = torch.stack([_image,_segment_image,_out_image],dim=0)
-                save_image(img,out_dir+"out.png")
+                save_image(img,out_dir+f"out{epoch+1}.png")
             
-            
+        end_time = time.time()
+        print("time cost:{:.2f}/epoch".format(end_time-start_time))
         if epoch % 3 == 0:
             if train_loss.item() < min_loss:
                 min_loss = train_loss.item()
